cd6a78288bca67cac5c647c6cd12ce64a6dc8984
yoresroy <yor_27@hotmail.com>
2023-01-12 13:30:20 -0600

    Add - manejo dinamico

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild('miFormulario') miFormulario! : NgForm;
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Rodrigo',
+    favoritos: [
+      { id : 1, nombre:'Metal Gear' },
+      { id : 2, nombre: 'DeathStranding'}
+    ]
+  }
+
+  nuevoJuego: string = '';
+
+  agregarJuego(){
+    const nuevoFavorito : Favorito = {
+      id: this.persona.favoritos.length + 1 ,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( { ...nuevoFavorito } ); 
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index:number){
+    this.persona.favoritos.splice( index, 1 ) 
+  }
+
+  guardar(){
+    console.log('Formulario posteado');
   }
 
 }
