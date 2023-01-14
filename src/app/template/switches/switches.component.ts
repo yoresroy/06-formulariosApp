30b2c4ba5ff1a84a8b6c65bca031cd2a3bda8a31
yoresroy <yor_27@hotmail.com>
2023-01-14 11:51:30 -0600

    Add - Formulario

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona = {
+    genero : 'F',
+    notificaciones: true
   }
+
+  terminosYCondiciones: boolean = false;
 
 }
