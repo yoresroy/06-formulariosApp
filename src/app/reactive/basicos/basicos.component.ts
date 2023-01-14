4ce46e0a7e51d1ef4800715d37ea8aed3687e8e2
yoresroy <yor_27@hotmail.com>
2023-01-14 12:46:24 -0600

    Add -  FormGroup

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  /*
 
-  ngOnInit(): void {
-  }
+  miFormulario: FormGroup = new FormGroup({
+    nombre        : new FormControl('RTX 4080ti'),
+    precio        : new FormControl(1500),
+    existencias   : new FormControl(5),
+  });
+
+  */
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre      : [ 'RTX 4080ti' ],
+    precio      : [ 0 ],
+    existencias : [ 0 ],
+  })
+
+  constructor( private formBuilder: FormBuilder ) { }
+
+
+
+
 
 }
