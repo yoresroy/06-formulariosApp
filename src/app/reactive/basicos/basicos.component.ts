6d0903bbb9b243f7eb5d9df94d098945cd33cc6d
yoresroy <yor_27@hotmail.com>
2023-01-14 22:18:04 -0600

    Add - FormBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormControlName, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -19,14 +19,17 @@ export class BasicosComponent {
   */
 
   miFormulario: FormGroup = this.formBuilder.group({
-    nombre      : [ 'RTX 4080ti' ],
-    precio      : [ 0 ],
-    existencias : [ 0 ],
+    nombre      : [ '', [Validators.required, Validators.minLength(3)]  ],
+    precio      : [ 0, [Validators.min(0), Validators.required] ],
+    existencias : [ 0, [Validators.min(0), Validators.required] ],
   })
 
   constructor( private formBuilder: FormBuilder ) { }
 
-
+  campoEsValido(campo: string){
+    return  this.miFormulario.controls[campo].touched
+    && this.miFormulario.controls[campo].errors;
+  }
 
 
 
