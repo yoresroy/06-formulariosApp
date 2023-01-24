5a01b98164070ca2f42e9710abd0f345f4b67d4d
yoresroy <yor_27@hotmail.com>
2023-01-23 23:44:42 -0600

    WIP - add Sumit y validación

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre      : [  , [Validators.required, Validators.minLength(3)]  ]
+  })
+  
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+
+  campoEsValido(campo: string){
+    return  this.miFormulario.controls[campo].touched
+    && this.miFormulario.controls[campo].errors;
+  }
+
+  guardar(){
+    if ( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
   }
 
 }
